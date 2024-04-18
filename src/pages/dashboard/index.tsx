1b474172ca876dd153fbef808f0c9accdc6cab63
Wilian Brito <contato.beitsoft@gmail.com>
2024-04-18 11:37:05 -0300

    feat: add chart of the goals eplyeers

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,40 @@
-import { Container } from './styles'
+import { GoalEmployeesChart } from './components/GoalEmployeesChart'
+import SliderSizes from './components/Slider'
+import {
+  Container,
+  ContainerGoals,
+  ContentInfoTextAndMonthlyGoal,
+  GoalEmployeesChartContent,
+  InfoText,
+  MonthlyGoalChart,
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <ContainerGoals>
+        <ContentInfoTextAndMonthlyGoal>
+          <InfoText>
+            <h1>Oi, José!</h1>
+            <p>Veja as principais informações</p>
+            <p>da sua loja aqui!</p>
+          </InfoText>
+
+          <MonthlyGoalChart>
+            <header>Meta mensal - loja</header>
+            <SliderSizes />
+            <footer>R$570.000,00</footer>
+          </MonthlyGoalChart>
+        </ContentInfoTextAndMonthlyGoal>
+
+        <GoalEmployeesChartContent>
+          <header>Meta de Colaboladores</header>
+
+          <section>
+            <GoalEmployeesChart />
+          </section>
+        </GoalEmployeesChartContent>
+      </ContainerGoals>
     </Container>
   )
 }
--- src/pages/dashboard/styles.tsx
+++ src/pages/dashboard/styles.tsx
@@ -1,5 +1,58 @@
 import styled from '@emotion/styled'
 
 export const Container = styled.section`
+  margin-top: 6.25rem;
+  padding: 1.25rem 3.125rem;
   height: 100hv;
+`
+
+export const ContainerGoals = styled.section`
+  display: grid;
+  grid-template-columns: 1.5fr 1fr;
+  gap: 4rem;
+`
+
+export const Card = styled.section`
+  background:
+    linear-gradient(#ffffff, #ffffff) padding-box,
+    linear-gradient(to right, #ff9e00, #fff173) border-box;
+  border-radius: 30px;
+  border: 1px solid transparent;
+  padding: 1.875rem;
+
+  footer,
+  header {
+    font-weight: 500;
+  }
+`
+
+export const ContentInfoTextAndMonthlyGoal = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 2rem;
+`
+
+export const InfoText = styled.section`
+  h1 {
+    font-size: 28px;
+    font-weight: 400;
+  }
+  p {
+    font-size: 28px;
+    font-weight: 200;
+  }
+`
+
+export const MonthlyGoalChart = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  z-index: -11;
+`
+
+export const GoalEmployeesChartContent = styled(Card)`
+  section {
+    margin-top: 2rem;
+  }
 `
