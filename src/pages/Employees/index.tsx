4299da4495343e5f83b5e5b5b1088d1797b3645c
Wilian Brito <contato.beitsoft@gmail.com>
2024-04-23 12:03:37 -0300

    feat: add scale flow in footer of the page settings

--- src/pages/Employees/index.tsx
+++ src/pages/Employees/index.tsx
@@ -1,4 +1,4 @@
-import { Switch } from '@mui/material'
+import { Divider, Switch } from '@mui/material'
 import { useState } from 'react'
 import { CgPen } from 'react-icons/cg'
 
@@ -6,7 +6,7 @@ import { Button } from '@/components/Button'
 
 import ModalEditEmployee from './componentes/ModalEditEmployee'
 import { employess } from './employees'
-import { Container } from './styles'
+import { Container, ScaleFlowContainer } from './styles'
 
 export function Employees() {
   const [openModalEditEmpoyee, setOpenModalEditEmpoyee] = useState(false)
@@ -54,6 +54,31 @@ export function Employees() {
           ))}
         </tbody>
       </table>
+      <Divider component="div" role="presentation" />
+      <ScaleFlowContainer>
+        <header>
+          <h2>Fluxo Médio</h2>
+        </header>
+        <section>
+          <p>
+            Selecione o parâmetro de meses para o fluxo médio da sua escala.
+          </p>
+        </section>
+        <section className="buttons-flow">
+          <section>
+            <input type="radio" name="flow" id="month3" />
+            <label htmlFor="month3">3 meses</label>
+          </section>
+          <section>
+            <input type="radio" name="flow" id="month6" />
+            <label htmlFor="month6">6 meses</label>
+          </section>
+          <section>
+            <input type="radio" name="flow" id="year1" />
+            <label htmlFor="year1">1 ano</label>
+          </section>
+        </section>
+      </ScaleFlowContainer>
       <footer>
         <Button text="Salvar colaboladores" color="#000" bgColor="#ffe2b3" />
       </footer>
--- src/pages/Employees/styles.ts
+++ src/pages/Employees/styles.ts
@@ -14,6 +14,7 @@ export const Container = styled.section`
     border-collapse: separate;
     border-spacing: 0 1rem;
     margin-top: 1.5rem;
+    margin-bottom: 0.5rem;
 
     thead {
       td {
@@ -68,5 +69,47 @@ export const Container = styled.section`
     display: flex;
     justify-content: flex-end;
     margin-top: 4.375rem;
+  }
+`
+
+export const ScaleFlowContainer = styled.section`
+  margin-top: 1rem;
+  > header {
+    h2 {
+      font-weight: 400;
+    }
+  }
+  section {
+    margin-top: 1.5rem;
+    &.buttons-flow {
+      display: flex;
+      align-items: center;
+      gap: 4rem;
+
+      > section {
+        input[type='radio'] {
+          display: none;
+        }
+        > label {
+          width: 120px; /* Você pode ajustar este valor conforme necessário */
+          text-align: center;
+          display: block;
+        }
+
+        > input[type='radio'] + label {
+          border-radius: 50px;
+          background-color: transparent;
+          font-weight: 500;
+          border: 1px solid ${(props) => props.theme.orangeDark};
+          padding: 10px 15px;
+          cursor: pointer;
+        }
+
+        input[type='radio']:checked + label {
+          background-color: ${(props) => props.theme.orangeMedium};
+          color: #fff;
+        }
+      }
+    }
   }
 `
